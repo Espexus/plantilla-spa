c2d7ad01d933c064b2e17506ddc76d21d3b0fdc9
Jesus E <jesusespinosa10usb@gmail.com>
2026-04-26 19:31:22 -0500

    css 8, carrusel

--- slider.js
+++ slider.js
@@ -10,4 +10,9 @@ const observer = new IntersectionObserver((entries) => {
   threshold: 0.3
 });
 
+boton_instagram = document.getElementById('boton-instagram');
+boton_instagram.addEventListener('click', () => {
+  window.open('https://www.instagram.com/piel.duraznospa/', '_blank');
+});
+
 elementos.forEach(el => observer.observe(el));
