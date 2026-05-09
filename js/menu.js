8625b537a8ef9d5a1be985a1d5a0054ce137abc6
Jesus E <jesusespinosa10usb@gmail.com>
2026-05-09 17:26:09 -0500

    banner de publicidad

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,19 @@
 const menu = document.getElementById("menu");
 const botonMenu = document.getElementById("boton-menu");
 const botonCerrar = document.getElementById("boton-cerrar");
+const banner = document.getElementById("banner-promociones");
+const body = document.getElementById("body")
 
 botonMenu.addEventListener("click", () => {
     menu.classList.toggle("menu-oculto");
     botonCerrar.classList.toggle("boton-oculto");
+    banner.classList.toggle("oculto");
+    body.style.paddingTop = "0";
 });
 
 botonCerrar.addEventListener("click", () => {
     menu.classList.toggle("menu-oculto");
     botonCerrar.classList.toggle("boton-oculto");
+    banner.classList.toggle("oculto");
+    body.style.paddingTop = "30px";
 });
